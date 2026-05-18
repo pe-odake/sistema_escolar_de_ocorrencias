3c213308d76f22fdbdf9f5154a4a568c6ec76e11
Pedro Odake <pedroodake@gmail.com>
2026-05-18 19:09:17 -0300

    Base inicial do SpringBoot

---
+++ src/main/java/com/pedroodake/sistema_de_ocorrencia/SistemaDeOcorrenciaApplication.java
@@ -0,0 +1,13 @@
+package com.pedroodake.sistema_de_ocorrencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeOcorrenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeOcorrenciaApplication.class, args);
+	}
+
+}
